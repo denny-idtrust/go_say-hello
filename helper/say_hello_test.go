e7b32871286c479585f86ceda962a5805a7f30c8
denny tario <denny@idtrust.id>
2023-01-13 17:05:37 +0700

    say hello

--- helper/say_hello_test.go
+++ helper/say_hello_test.go
@@ -8,6 +8,34 @@ import (
 	"testing"
 )
 
+func TestTableHellorlf(t *testing.T) {
+	test := []struct {
+		name     string
+		status   string
+		request  string
+		expected string
+	}{
+		{
+			name:     "John",
+			status:   "Active",
+			request:  "Hi Hello John Active",
+			expected: "Hi Hello John Active",
+		},
+	}
+}
+
+func TestSubTest(t *testing.T) {
+	t.Run("John", func(t *testing.T) {
+		result := SayHello("John", "Active")
+		assert.Equal(t, "Hi Hello John Active", result)
+	})
+
+	t.Run("Denny", func(t *testing.T) {
+		result := SayHello("Denny", "Active")
+		assert.Equal(t, "Hi Hello Denny Active", result)
+	})
+}
+
 func TestMain(m *testing.M) {
 	fmt.Println("Before Unit Test")
 	m.Run()
